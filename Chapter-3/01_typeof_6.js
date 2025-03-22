c1bd4797558dbd9f646213cda933c9cd96539ca7
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 20:32:59 +0600

    added typeof operator related problems at chapter 3

---
+++ Chapter-3/01_typeof_6.js
@@ -0,0 +1,25 @@
+//problem 1
+const price = 555;
+// console.log(typeof price);
+
+//problem 2
+const learning = true;
+// console.log(learning);
+// console.log(typeof learning);
+
+//problem 3
+const megaCity = "Dhaka";
+// console.log(typeof megaCity);
+
+//problem 4
+const temperature = 102.5;
+const variableType = typeof temperature;
+// console.log(variableType);
+
+//problem 5
+const isHungry = true;
+// console.log(typeof isHungry);
+
+//problem 6
+const isReach = true;
+// console.log(typeof isReach);
