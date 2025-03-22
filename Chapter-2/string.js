bfcd019880e96be7550b4faae5a60020e64fe1bd
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 19:26:31 +0600

    added string related problems at chapter 2

---
+++ Chapter-2/string.js
@@ -0,0 +1,20 @@
+const name = "Mahbubur Rahman";
+console.log(name);
+
+const address = "Hemayetpur, Savar, Dhaka";
+console.log(address);
+
+const univOfBestFriend = "Alrounder University of Wife Materials";
+console.log(univOfBestFriend);
+
+const fan = "Kate Winslet";
+console.log(fan);
+
+const movieOfTheYear = "The Unbreakable Boy";
+console.log(movieOfTheYear);
+
+const hobby = "Travelling";
+console.log(hobby);
+
+const color = "Navy Blue";
+console.log(color);
