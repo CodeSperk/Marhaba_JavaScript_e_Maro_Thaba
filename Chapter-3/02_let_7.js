de1e7e618782249d31c24959dca05aec2a84d347
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 20:41:01 +0600

    added let related problems

---
+++ Chapter-3/02_let_7.js
@@ -0,0 +1,36 @@
+//problem 1
+let age = 25;
+console.log(age);
+age = 26;
+console.log(age);
+
+//problem 2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+//problem 3
+let studyTime = 4;
+studyTime = 8;
+console.log(studyTime);
+
+//problem 4
+let friendsCount = 3;
+friendsCount -= 2;
+console.log(friendsCount);
+
+//problem 5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+//problem 6
+let chairesCount = 40;
+chairesCount -= 2;
+chairesCount -= 6;
+console.log(chairesCount);
+
+//problem 7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
