73334e9466c01664246be75c7aaa10e4f10a29ab
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 20:23:57 +0600

    added math related problems at chapter 2

---
+++ 05_math_10.js
@@ -0,0 +1,56 @@
+// Problem 1
+const income = 100;
+const cost = 40;
+const remaining = income - cost;
+
+// problem 2
+const totalPencil = 10;
+const pencilAtSingleTime = 2;
+const totalUsableTimes = totalPencil / pencilAtSingleTime;
+
+//problem 3
+const cash = 60;
+const costEachTime = 15;
+const times = cash / costEachTime;
+const remainingAmount = cash - times * costEachTime;
+
+//problem 4
+const str1 = "Hello";
+const str2 = "World";
+const strSum = str1 + str2;
+
+//problem 5
+const riceKG = 153;
+const lentilsKG = 261;
+const totalKG = riceKG + lentilsKG;
+
+// console.log(remaining, totalUsableTimes, remainingAmount, totalKG, strSum);
+
+//problem 6
+const totalAmount = 500;
+const eachTimeCost = 75;
+const numberofCostTimes = parseInt(500 / 75);
+const remainder = totalAmount % eachTimeCost;
+// console.log(numberofCostTimes, remainder);
+
+//problem 7
+const dividend = parseInt(8 / 3);
+const remainder7 = 8 % 3;
+// console.log(dividend, remainder7);
+
+//problem 8
+const remainder8 = 50 % 9;
+// console.log(remainder8);
+
+//problem 9
+const countryName = "Bangla" + "desh";
+// console.log(countryName);
+
+//problem 10
+const totalRiceKg = 12 * 40;
+const requiredEachDay = 4;
+const totalDays = parseInt(totalRiceKg / requiredEachDay);
+const months = parseInt(totalDays / 30);
+const remainingDays = totalDays % 30;
+// console.log(totalDays);
+// console.log(months + " months ," + remainingDays + " days");
