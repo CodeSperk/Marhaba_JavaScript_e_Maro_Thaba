9fcd839a6b883c3ee8bc8561f15266e4a0d821c7
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 21:05:34 +0600

    added comment related problems

---
+++ Chapter-3/03_comment_7.js
@@ -0,0 +1,42 @@
+//problem 1 : Final exam score
+const score = 65;
+
+//problem 2
+//readious of a playground
+let radius;
+
+//assign radius value in feet
+radius = 156;
+
+//calculate area of the playground
+const area = (radius * radius) & 3.14159;
+console.log(area);
+
+//problem 3
+const dress = 4;
+/* this is the dress that 
+i bought for this year 
+Eid celebration */
+
+//problem 4
+const giftMoney = 100; //gifted for good handwriting
+
+//problem 5
+const studyTime = 7;
+/*
+  I will complete the chapter 3 &
+  chapter 4 of Marhaba JavaScript e Maro Thaba.
+  then another 3 hours tomorrow will practice these chapters
+*/
+
+//problem 6
+const sendMoney = 1000;
+/*
+  1000 will be sent.
+  aditionally service charge 5 will be cut from the 
+  sender
+*/
+
+//problem 7
+//product price is 545 and delivery charge is 93
+const pay = 638;
