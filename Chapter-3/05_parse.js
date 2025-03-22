5732bc82e2fdf4d3a3362be3883ea9d529e6198a
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 22:10:15 +0600

    added parse related problems

---
+++ Chapter-3/05_parse.js
@@ -0,0 +1,27 @@
+//problem 1
+const num = "20";
+const parsedNum = parseInt(20);
+const sum = parsedNum + 10;
+
+//problem 2
+const val = "3.14159";
+const result = parseInt(val);
+
+//problem 3
+const toNum = parseInt("premikBoy");
+
+//problem 4
+const num1 = 3.456;
+const num2 = 2.789;
+const sumResult = parseFloat((num1 + num2).toFixed(2));
+
+//problem 5
+const actualSum = parseFloat(56.78) + parseFloat(12.34);
+
+//problem 6
+const number1 = parseFloat(parseFloat("10.5678").toFixed(2));
+const number2 = parseFloat(parseFloat("5.4321").toFixed(2));
+
+//problem 7
+const round = parseFloat(parseFloat("42.45689754").toFixed(3));
+console.log(round);
