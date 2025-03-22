17d8a2206628d69ed481514835faefe6fa2b660d
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 22:42:47 +0600

    added comparison related problems

---
+++ Chapter-4/02_comparison.js
@@ -0,0 +1,42 @@
+// problem 1
+const myAge = 15;
+const youngerAge = 10;
+const isYoungerYoungThanMe = youngerAge < myAge;
+
+// problem 2
+const marks = 45;
+const passMarks = 33;
+const isPassed = passMarks >= 33;
+
+// problem 3
+const jambura = 34;
+const kadBell = 33;
+
+const isJamburaCheeper = jambura < kadBell;
+
+// problem 4
+const me = 10;
+const friend = 10;
+const isEqual = me === friend;
+
+// problem 5
+const myBiskut = 1;
+const myFriendBiskut = 20;
+
+const isMineLess = myBiskut < myFriendBiskut;
+
+// problem 6
+const realMadrid = 2;
+const barsa = 3;
+const isMadridLessThanBarsa = realMadrid < barsa;
+
+// problem 7
+const myName = "Rahim";
+const myFriendName = "Karim";
+
+const isSame = myName === myFriendName;
+
+// problem 8
+const meStudied = true;
+const friendStudied = false;
+const isBothSame = meStudied === friendStudied;
--- comparison.js
+++
@@ -1,3 +0,0 @@
-const myAge = 15;
-const youngerAge = 10;
-const isYoungerYoungThanMe = 
