b67e30a3c7af9dcf39aebc907a51db99236cad88
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 19:19:30 +0600

    added console related probles at chapter 2

---
+++ Chapter-2/console.js
@@ -0,0 +1,21 @@
+const age = 25;
+console.log(age);
+
+const booksToRead = "Learning JavaScript Data Structures and Algorithms";
+console.log(booksToRead);
+
+let wakeUpTime = "7:30";
+wakeUpTime = "8:30";
+console.log(wakeUpTime);
+
+const wastedTime = "1hr";
+console.log(wakeUpTime);
+
+const borrow = 5000;
+console.log(borrow);
+
+const goals = 5;
+console.log(goals);
+
+const pillow = 4;
+console.log(pillow);
