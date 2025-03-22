48338dcea2843d8b12927f587da2fd32d3f18465
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 22:33:07 +0600

    added direct comparison related problems

---
+++ Chapter-4/01_directComparison.js
@@ -0,0 +1,25 @@
+// problem 1
+const isMeMoreThanYounger = 7 > 5;
+const isYoungerMoreThanMe = 5 > 7;
+// console.log(isYoungerMoreThanMe);
+
+// problem 2
+const isMyClassLarger = 45 > 50;
+const isOtherClassLarger = 50 > 45;
+
+// problem 3
+const isMeMoreThanFriend = 75 > 75;
+const isFriendMoreThanMe = 75 > 75;
+
+// problem 4
+const isTargetAchieved = 100 === 75;
+
+// problem 5
+const isNotEqual = 10 !== 7;
+// console.log(isNotEqual);
+
+// problem 6
+const isEqualOrLess = 20 <= 15;
+
+// problem 7
+const isFriendGotLessThanMine = 45 > 97;
---
+++ comparison.js
@@ -0,0 +1,3 @@
+const myAge = 15;
+const youngerAge = 10;
+const isYoungerYoungThanMe = 
