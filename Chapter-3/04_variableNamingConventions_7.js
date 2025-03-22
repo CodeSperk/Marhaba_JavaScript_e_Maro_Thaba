612ab29a2c7dbbebab8b4873255a5b7485b159bf
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-22 21:45:33 +0600

    added namingConventions related problems

---
+++ Chapter-3/04_variableNamingConventions_7.js
@@ -0,0 +1,25 @@
+// problem 1
+const favoriteFood = "Kacchi Biriany";
+
+// problem 2
+const city = "Chitagong";
+const City = "Dhaka";
+console.log(City);
+
+// problem 3
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName);
+
+// problem 4
+const homeAddress = "12/4, Pallabi, Dhaka";
+
+// problem 5
+const super = 12; //can't use keywords to declare variable
+
+// problem 6
+const class = "six"; //keywords cann't be used
+
+// problem 7
+//variable can't be started with number
+const 2ndPosition = "Rajjak"
