9a1f45a10a178f9c3891ec1c8e62898a5540d123
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-24 00:52:53 +0600

    added bracket and dot notation related practice problems

---
+++ C8_Object/01_Object.js
@@ -0,0 +1,55 @@
+//problem 1
+const teacher = {
+  name: "Prof. Abdur rahim",
+  subject: "History",
+  age: "38",
+  address: "32/4, Shahbag, Dhaka-1000",
+};
+
+//problem 2
+const tree = {
+  name: "Mango Tree",
+  Height: "38 feet",
+  age: 7,
+  fruitName: "Mango",
+};
+
+//problem 3
+const cat = {
+  name: "Ginger",
+  color: "Darkish",
+  age: 3,
+  weight: 2.5,
+};
+
+//problem 4
+const father = {
+  name: "Sajjad Sarkar",
+  occupation: "Sergent",
+  age: 54,
+  children: 4,
+};
+
+//problem 5
+const motorBike = {
+  brand: "Suzuki",
+  tire: 2,
+  color: "Dark",
+  maxSpeed: "75k/h",
+};
+
+//problem 6
+const bird = {
+  name: "Magpie",
+  color: "Yello and sky blue",
+  food: "broken rice",
+};
+
+//problem 7
+const laptop = {
+  brand: "Asus",
+  processor: "Interl core i5",
+  ram: "16GB",
+  price: 65000,
+  screen: "21 inch",
+};
