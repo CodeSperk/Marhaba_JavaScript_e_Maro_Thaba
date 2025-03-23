85bc79a8441f34ca62af692f8dd4dbbba254310d
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-23 19:49:28 +0600

    added while loop related problems

---
+++ Chapter-7/02_while_loop.js
@@ -0,0 +1,33 @@
+// problem 1
+let i = 0;
+while (i <= 10) {
+  console.log("I will practice programming at least 3 hours everyday");
+  i++;
+}
+
+// problem 2
+let num = 1;
+while (num <= 20) {
+  console.log(num);
+  num++;
+}
+
+// problem 3
+let n = 50;
+while (n <= 100) {
+  console.log(n);
+  n++;
+}
+
+// problem 4
+let x = 1;
+while (x <= 10) {
+  console.log(3 + " * " + x + " = " + x * 3);
+  x++;
+}
+// problem 5
+let z = 101;
+while (z <= 110) {
+  console.log(z + " / " + 2 + " = " + z / 2);
+  z++;
+}
