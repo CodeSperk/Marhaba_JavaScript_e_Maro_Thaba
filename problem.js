51a12a086c5b696e74522d3d2062ee5b6dc714e1
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-23 18:41:22 +0600

    added simple array related problems

---
+++ problem.js
@@ -0,0 +1,40 @@
+// problem 1
+const nums = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+// problem 2
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Guava"];
+
+// problem 3
+const vegetables = [
+  "Potato",
+  "Onion",
+  "Mula",
+  "Carrot",
+  "Tomato",
+  "Dherosh",
+  "Laow",
+  "Cucumber",
+  "Potol",
+  "Begun",
+];
+const length = vegetables.length;
+
+// problem 4
+const favouriteMovies = [
+  "The Titanic",
+  "The Last Airbender",
+  "Mucize",
+  "3 Ediots",
+  "12th Fail",
+];
+
+// problem 5
+const oddNums = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+// problem 6
+const colors = ["white", "black", "red", "yellow", "Orange"];
+console.log(colors.length);
+
+// problem 7
+const capitals = ["Dhaka", "Delhi", "Islamabad", "Kabul"];
+console.log(capitals.length);
