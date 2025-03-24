d307e7e153c2a965db5810e11bb96a3c7b7c1d1c
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-25 05:06:22 +0600

    added arr function related problems

---
+++ Practice_Problems_01/01_template_literal.js
@@ -0,0 +1,42 @@
+//problem 01
+const num1 = 20;
+const num2 = 10;
+// const result = `Difference of ${num1} & ${num2} is ${num1 - num2}`;
+
+//problem 02
+const employee = {
+  name: "Mahbub",
+  age: 25,
+  salary: 20000,
+};
+// const result = `Age & Salary of ${employee.name} are ${employee.age} & ${employee.salary}/= `;
+
+//problem 03
+const fruits = ["Apple", "Banana", "Orange"];
+// const result = `One of my favourite fruits is ${fruits[2]}`;
+
+//problem 04
+const a = 50;
+const b = 10;
+// const result = `The division of ${a} & ${b} is ${a - b}`;
+
+//problem 05
+const person = {
+  firstName: "Amit",
+  lastName: "Hasan",
+};
+// const result = `Full Name : ${person.firstName} ${person.lastName}`;
+
+//problem 06
+const animals = ["Cat", "Dog", "Elephant"];
+// const result = `My favourite animals are ${animals[0]}, ${animals[1]} & ${animals[2]}!`;
+
+//problem 7
+const student = {
+  name: "Mahbubur Rahman",
+  age: 25,
+  result: [45, 46, 47],
+};
+const result = `Average marks of ${student.name}(${student.age}) is : ${
+  (student.result[0] + student.result[1] + student.result[2]) / 3
+}`;
---
+++ Practice_Problems_01/03_arrow_function.js
@@ -0,0 +1,23 @@
+//problem 1
+const getFirstValue = (arr) => arr[0];
+// const result = getFirstValue([23, 22, 26]);
+
+//problem 2
+const getMultiplication = (x, y, z) => x + y + z;
+// const result = getMultiplication(21, 2, 12);
+
+//problem 3
+const unknownFunc = () => "Unknown";
+// const result = unknownFunc();
+
+//problem 4
+const netMoney = (obj) => obj.money / 5;
+// const result = netMoney((calculate = { name: "Mahbub", money: 500 }));
+
+//problem 5
+const sumOfMarginalValue = (arr) => arr[0] + arr[arr.length - 1];
+// const result = sumOfMarginalValue([34, 23, 12, 10]);
+
+//problem 6
+const getAddition = (a = 10, b = 5) => a + b;
+// const result = getAddition();
