11aa2c55565b35cb896ba8e5ff80655c9e6fe16c
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-25 17:39:32 +0600

    added some & every methods related problems

---
+++ C13-Array_Methods-30/02_some_every.js
@@ -0,0 +1,12 @@
+// problem 1
+const price = [30, 45, 120, 60, 10];
+const greaterThanHundred = price.some((item) => item > 100);
+
+// problem 2
+const nums = [12, 43, 12, 23, 14, 17, 45];
+const isDivisableBy5 = nums.every((num) => num % 5 === 0);
+
+// problem 3
+const ages = [23, 17, 18, 20];
+const isAdult = ages.every((age) => age > 18);
+console.log(isAdult);
--- problem.js
+++ problem.js
@@ -1,14 +1,15 @@
 // problem 1
-const price = [30, 46, 20, 60, 10];
-const increaseBy10 = price.map((item) => item + 13);
+const price = [30, 45, 120, 60, 10];
+const greaterThanHundred = price.some((item) => item > 100);
 
 // problem 2
-const names = ["Messi", "Maradona", "Pele", "Zidane", "Ronaldo"];
-const longerNames = names.filter((name) => name.length > 5);
+const nums = [12, 43, 12, 23, 14, 17, 45];
+const isDivisableBy5 = nums.every((num) => num % 5 === 0);
 
 // problem 3
-const nums = [12, 43, 12, 23, 14, 17, 45];
-const greaterThan20 = nums.find((num) => num > 20);
+const ages = [23, 17, 18, 20];
+const isAdult = ages.every((age) => age > 18);
+console.log(isAdult);
 
 // problem 4
 const height = [43, 53, 64, 26, 74];
@@ -16,7 +17,7 @@ const tallerThan69 = height.filter((p) => p > 69);
 
 // problem 5
 const numbers = [7, 10, 15, 20, 25, 30];
-numbers.map((num) => console.log(num / 3));
+// numbers.map((num) => console.log(num / 3));
 
 // problem 6
 const friends = [
@@ -34,8 +35,8 @@ const startsWithH = persons.filter((name) => name[0] === "H");
 
 // problem 8
 const newNums = [1, 2, 3, 4, 5];
-newNums.forEach((n) => console.log(n));
+// newNums.forEach((n) => console.log(n));
 
 // problem 9
 const animals = ["Cow", "Dog", "Sheep", "Horse"];
-animals.forEach((animal) => console.log(animal));
+// animals.forEach((animal) => console.log(animal));
