08f2a318d231a965d7f8937fa88a7d18e535b029
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-25 18:48:08 +0600

    added slice & splice realted problems

---
+++ C13-Array_Methods-30/06_slice_splice.js
@@ -0,0 +1,15 @@
+// problem 1
+const fruits = ["apple", "banana", "watermelon", "guava"];
+const sliced = fruits.slice(1, 3);
+
+// problem 2
+const car = ["Tesla", "BMW", "Toyotas", "Ford"];
+const slicedCars = car.slice(0, 2);
+
+//problem 3
+const movieList = ["Inception", "Titanic", "Joker", "Avatar", "Interstellar"];
+movieList.splice(2, 2, "Batman", "Superman");
+
+//problem 4
+const players = ["Messi", "Ronaldo", "Neymar", "Mbappe"];
+players.splice(2, 1, "Halland");
