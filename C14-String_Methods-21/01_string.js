945140c37836081a9138f16f6307d538371428d0
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-29 23:44:26 +0600

    added string methods related problems

---
+++ C14-String_Methods-21/01_string.js
@@ -0,0 +1,20 @@
+//problem 1
+const city = "Chattogram";
+const positionOfG = city.indexOf("g");
+
+//problem 2
+const division = "Sylhet";
+const hasY = division.includes("y");
+
+//problem 3
+const name = "Rifat";
+const lastChar = name[name.length - 1]
+
+//problem 4
+const language = "javascript"
+ const ripStartedAt = language.indexOf("rip");
+
+//problem 5
+const text = "Immutable";
+const textLength = text.length;
+text[2] = "p"; //Immutable
