2469099e70e7b0a8269f75d128e84263ed8f7543
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-30 01:22:26 +0600

    added undefined related problems

---
+++ C15-JS_Core_Concept-27/02_undefined.js
@@ -0,0 +1,30 @@
+//problem 1
+let name;
+console.log(name);
+
+//problem 2
+const addToNum = (a , b) =>{
+    const sum = a + b;
+}
+console.log(addToNum(10 + 20));
+
+//problem 3
+const arr = [10, 20, 30, 40, 50];
+delete arr[2]
+console.log(arr[2]);
+
+//problem 4
+function sum(a, b, c) {
+    console.log(c);
+}
+sum(10,20);
+
+
+//problem 5
+const student = {
+    name: "serious sajib",
+    roll: 1,
+    masks: 99
+};
+
+console.log(student.marks);
