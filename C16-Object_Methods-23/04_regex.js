49309af918bf3a79c318e68a6bdd6d04b35ea134
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-30 04:39:07 +0600

    added regex related problems

---
+++ C16-Object_Methods-23/04_regex.js
@@ -0,0 +1,12 @@
+//problem 1
+const sentence = "I bought an orange";
+const changed = sentence.replace(/orange/g, "banana");
+
+//problem 2
+const sentence2 = "I like to have apple and banana";
+const hasAna = /ana/.test(sentence2);
+// console.log(hasAna);
+
+//problem 3
+const sentence3 = "I am eating apple, apple is good. apple helps me a lot";
+const changedSentence = sentence3.replace(/apple/g, "JavaScript");
--- practice.js
+++
@@ -1,8 +0,0 @@
-//Compare two date and get result in day
-
-const date1 = new Date("2018-11-01");
-const date2 = new Date("2019-10-01");
-
-const differenceInmilliSecond = date2 - date1;
-const differenceInDay = differenceInmilliSecond/(1000 * 60 * 60 * 24)
-console.log(differenceInDay);
