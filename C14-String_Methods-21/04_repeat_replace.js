c01392c17ac8f1a7947ba7a14d2933f511e87794
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-30 00:18:54 +0600

    added replace, replaceAll & repeat string methods related problems

---
+++ C14-String_Methods-21/04_repeat_replace.js
@@ -0,0 +1,12 @@
+// problem 1
+const text = "Hi";
+const repeatedText = text.repeat(10);
+
+// problem 2
+const text2 = "I am learning JavaScript, JavaScript is amazing";
+const replacedJs = text2.replace("JavaScript", "JS")
+
+// problem 3
+const text3 = "I love apples and apples are tasty";
+const replacedText = text3.replaceAll("apples", "mangoes")
+
