8db2fb9cb720682d1f58dbe6408fb1bd0a31e998
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-30 03:41:36 +0600

    add Math methods related problems

---
+++ C15-JS_Core_Concept-27/tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+//problem 1
+let num = 15;
+let copy = num;
+copy = 25;
+// console.log(num, copy);
+
+
+//problem 2
+const arr = [1, 2, 3]
+let copyArr = arr;
+copyArr.push(88);
+// console.log(arr, copyArr);
+
+
+//problem 3
+const language = {
+    name : "JS",
+    age: 30
+}
+let newVersion = language;
+newVersion.location = "Browser";
+// console.log(language, newVersion);
---
+++ C16-Object_Methods-23/01_constructior.js
@@ -0,0 +1,19 @@
+//problem 1
+const start = "786";
+const numbered1 = Number(start);
+const numbered2 = +start;
+
+//problem 2
+const willAttend = 0;
+const booleaned = Boolean(willAttend);
+// console.log(typeof booleaned);
+
+//problem 3
+const place = new Object();
+place.city = "Dhaka",
+place.country = "Bangladesh";
+
+//problem 4
+const total = 1970;
+const strValue1 = String(total);
+const strValue2 = total + "";
