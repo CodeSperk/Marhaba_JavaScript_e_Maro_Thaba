e3e16ccde87cb00592012273939afe6115f793c0
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-29 23:52:35 +0600

    added case related string methods

---
+++ C14-String_Methods-21/02_case_methods.js
@@ -0,0 +1,16 @@
+//problem 1
+const email = "Inbx.mahbub@gmail.com";
+const lowerCasedEmail = email.toLowerCase();
+
+//problem 2
+const greeting = "hELlo WoRLd";
+const upperGreeting = greeting.toUpperCase()
+const lowerGreeting = greeting.toLowerCase()
+
+//problem 3
+const language = "JavaScript";
+const hasScript = language.toLowerCase().includes("script");
+
+//problem 4
+const text = "NodeJs";
+const isCapitalized = text[0] === text.toUpperCase()[0]
