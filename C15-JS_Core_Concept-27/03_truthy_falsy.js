47d564112b77a676a3328ad916d9012ae8a8caa0
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-30 01:33:44 +0600

    added truthy vs falsy related problems

---
+++ C15-JS_Core_Concept-27/03_truthy_falsy.js
@@ -0,0 +1,18 @@
+//problem 1
+if("false"){
+    console.log("True");
+}
+
+//problem 2
+if({}){
+    console.log("True");
+}
+
+//problem 3
+if([]){
+    console.log("True");
+}
+
+//problem 4
+const result = !![]; //true
+console.log(result);
